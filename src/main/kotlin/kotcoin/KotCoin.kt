102764bc49ecd38ab2ddd872bd988093283a905a
Adrian Marszalek <adrian.marszalek@bitcomp.fi>
2018-03-27 14:59:45 +0200

    http endpoints update

--- src/main/kotlin/kotcoin/KotCoin.kt
+++ src/main/kotlin/kotcoin/KotCoin.kt
@@ -1,17 +1,16 @@
 package kotcoin
 
 import io.javalin.Javalin
-import java.util.*
 
 val kotcoin = Blockchain
 
 fun main(args: Array<String>) {
     val app = Javalin.start(7000)
-    app.get("/chain") { ctx ->
+    app.get("/blocks") { ctx ->
         ctx.json(kotcoin.chain)
-    }.post("/mine") { ctx ->
-        val minedBlock = kotcoin.mineBlock(UUID.randomUUID())
+    }
+    app.post("/blocks/mine") { ctx ->
+        val minedBlock = kotcoin.mineBlock(ctx.body())
         ctx.json(minedBlock)
     }
-
 }
